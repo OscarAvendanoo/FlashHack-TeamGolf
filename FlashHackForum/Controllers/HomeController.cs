9d045138589fc71d8346eaeb4f0de6ba50fd3dfc
Oscar avendano <Oscaravendnaoo@hotmail.com>
2025-02-19 19:08:39 +0100

    laddar upp projektmall

---
+++ FlashHackForum/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using FlashHackForum.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FlashHackForum.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
