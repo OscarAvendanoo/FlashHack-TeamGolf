d4f106f78b3f0c6317f77aa769f1277018f7b330
Kevin Hedendahl <kevin.hed@hotmail.com>
2025-02-26 10:23:31 +0100

    Update validation

--- FlashHackForum/ViewModels/CreateEditSubCategoryViewModel.cs
+++ FlashHackForum/ViewModels/CreateEditSubCategoryViewModel.cs
@@ -8,8 +8,9 @@ namespace FlashHackForum.ViewModels
         [Required(ErrorMessage ="You have to pick a main category")]
         public int? MainCategoryId { get; set; }
 
+        
         [Required(ErrorMessage = "You have to enter a name for sub category")]
-        [StringLength(40, MinimumLength =2)]
+        [StringLength(40, ErrorMessage = "Name must at least 2 characters", MinimumLength = 2)]
         public string? Name { get; set; }
 
         public IEnumerable<MainCategory>? MainCategories { get; set; }
