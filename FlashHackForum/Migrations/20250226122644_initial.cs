9ea1cda1e96aa019aff051f928652ae420f5e735
Jonaandersson <jonaandersson1996@gmail.com>
2025-02-26 18:06:34 +0100

    MainCat-button

---
+++ FlashHackForum/Migrations/20250226122644_initial.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlashHackForum.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SecondCategories_MainCategories_MainCategoryId",
+                table: "SecondCategories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MainCategoryId",
+                table: "SecondCategories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SecondCategories_MainCategories_MainCategoryId",
+                table: "SecondCategories",
+                column: "MainCategoryId",
+                principalTable: "MainCategories",
+                principalColumn: "MainCategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SecondCategories_MainCategories_MainCategoryId",
+                table: "SecondCategories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MainCategoryId",
+                table: "SecondCategories",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SecondCategories_MainCategories_MainCategoryId",
+                table: "SecondCategories",
+                column: "MainCategoryId",
+                principalTable: "MainCategories",
+                principalColumn: "MainCategoryId");
+        }
+    }
+}
