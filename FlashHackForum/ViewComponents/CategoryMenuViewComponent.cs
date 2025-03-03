50b707519fd6f538e55a6bd1c37bd668bf0d8712
Kevin Hedendahl <kevin.hed@hotmail.com>
2025-03-03 14:29:25 +0100

    Fix list threads

--- FlashHackForum/ViewComponents/CategoryMenuViewComponent.cs
+++ FlashHackForum/ViewComponents/CategoryMenuViewComponent.cs
@@ -1,21 +1,25 @@
 ﻿using FlashHackForum.Data.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using FlashHackForum.Data;
+using FlashHackForum.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlashHackForum.ViewComponents
 {
     public class CategoryMenuViewComponent : ViewComponent
     {
-        private readonly IMainCategoryRepository _mainCategoryRepository;
 
-        public CategoryMenuViewComponent(IMainCategoryRepository mainCategoryRepository)
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryMenuViewComponent(ApplicationDbContext dbContext)
         {
-            _mainCategoryRepository = mainCategoryRepository;
+            
+            this.dbContext = dbContext;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var mainCategories = await _mainCategoryRepository.GetAllAsync();
+            var mainCategories = await dbContext.MainCategories.Include(mc=>mc.SecondCategories).ToListAsync();
             return View(mainCategories);
         }
     }
