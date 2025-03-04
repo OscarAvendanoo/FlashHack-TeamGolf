0fe6dd54af681f9b0f7cf1560e2900d89f96b2f1
Kevin Hedendahl <kevin.hed@hotmail.com>
2025-03-04 16:15:05 +0100

    Create MainPage

--- FlashHackForum/Migrations/20250304070219_init.cs
+++ FlashHackForum/Migrations/20250304070219_init.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace FlashHackForum.Migrations
 {
     /// <inheritdoc />
-    public partial class initial : Migration
+    public partial class init : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -239,7 +239,7 @@ namespace FlashHackForum.Migrations
                         column: x => x.ForumThreadId,
                         principalTable: "ForumThreads",
                         principalColumn: "ForumThreadID",
-                        onDelete: ReferentialAction.Restrict);
+                        onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateIndex(
---
+++ FlashHackForum/ViewModels/ForumThreadPostVM.cs
@@ -0,0 +1,10 @@
+﻿using FlashHackForum.Models;
+
+namespace FlashHackForum.ViewModels
+{
+    public class ForumThreadPostVM
+    {
+        public string MainCategoryName { get; set; }
+        public List<SubCategoryVM> SecondCategories { get; set; }
+    }
+}
